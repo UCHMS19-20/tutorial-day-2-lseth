1084e2ba499ba5ef89ed0e4423e1e99cc9c4fb15
laura <lseth@ucvts.org>
2019-12-09 10:27:28 -0500

    changed pic from ball to plane

--- bouncy.py
+++ bouncy.py
@@ -4,7 +4,7 @@ import pygame
 # start running pygame
 pygame.init()
 
-width, height = 400, 300
+width, height = 800, 600
 # speed is a list because values need to be able to change
 speed = [1, 1]
 # colors are tuples because values don't change
@@ -15,8 +15,8 @@ white = (255, 255, 255)
 screen = pygame.display.set_mode( (width, height) )
 
 # Load an image
-ball = pygame.image.load("img/ball.png")
-ballrect = ball.get_rect()
+plane = pygame.image.load("img/plane.png")
+planerect = plane.get_rect()
 
 while True:
     # loop through every event in the event queue
@@ -25,15 +25,15 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
     
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
+    planerect = planerect.move(speed)
+    if planerect.left < 0 or planerect.right > width:
         speed[0] *= -1
-    if ballrect.top < 0 or ballrect.bottom > height:
+    if planerect.top < 0 or planerect.bottom > height:
         speed[1] *= -1
 
     # Fill the screen with white
     screen.fill(white)
-    # Draw the ball with the size and location specified by ballrect
-    screen.blit(ball, ballrect)
+    # Draw the plane with the size and location specified by planerect
+    screen.blit(plane, planerect)
     # Update the display
     pygame.display.flip()
